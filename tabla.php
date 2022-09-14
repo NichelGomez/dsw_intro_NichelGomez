8ff1627140662293d65fc9ba1c312ef2456ba9cd
NichelGomez <nichel8080@gmail.com>
2022-09-14 11:40:05 +0100

    Create tabla.php

---
+++ tabla.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Tabla en PHP</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>14/09/2022</td>
+                <td>11:39</td>
+            </tr>
+        </table>
+    </body>
+</html>
